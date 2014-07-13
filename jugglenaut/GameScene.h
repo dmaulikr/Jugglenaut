07330d7903b42893d5207800bd5419ba4d38f2d5
Nicholas Trampe <nicholas@nicholas-pc.att.net>
2014-07-13 09:38:06 -0500

    Initial Commit

---
+++ jugglenaut/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  jugglenaut
+//
+
+//  Copyright (c) 2014 Off Kilter Studios. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ jugglenaut/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  jugglenaut
+//
+
+//  Copyright (c) 2014 Off Kilter Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
